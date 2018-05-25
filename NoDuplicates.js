bcfde0bd23aadb12540e04d7538cc2cfe7ebe310
HaydenCogburn <39624490+HaydenCogburn@users.noreply.github.com>
2018-05-25 08:36:19 -0500

    Create NoDuplicates.js

---
+++ NoDuplicates.js
@@ -0,0 +1,29 @@
+/*
+Objective is to remove all dublicate words from string
+*/
+
+function noDuplicates(phrase){
+    var output = "";
+    copies = 0;
+    var words = phrase.split(" ");
+    words.sort();
+    for (i = 0; i < words.length; i ++) {
+        if (words[i] == words[i + 1]) {
+            copies += 1;
+        }
+        
+    }
+    if (copies > 0) {
+        output = "no";
+    } 
+    else {
+        output = "yes";
+    }
+    return(output);
+}
+
+var output = noDuplicates("IN THE RAIN AND THE SNOW");
+console.log(output);
+
+var output = noDuplicates("THE RAIN IN SPAIN");
+console.log(output);
